87e40087ae6ba8f5e8056fd655032a8d508a2cd0
Zeqi Qiu <zeqiq@andrew.cmu.edu>
2018-10-22 10:50:51 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CplusplusCodeLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CplusplusCodeLib.dir/main.cpp.o"
+  "CplusplusCodeLib.pdb"
+  "CplusplusCodeLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CplusplusCodeLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
