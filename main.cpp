28b17b46460b12ba1c8f661cd9ac9a2d62dea48e
Zeqi Qiu <zeqiq@andrew.cmu.edu>
2018-10-22 11:17:20 -0400

    first

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+template <typename T>
+void print(T value){
+    std::cout<<value<<" size: "<<sizeof(value)<<std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    double a = 1;
+    print(a);
     return 0;
 }
